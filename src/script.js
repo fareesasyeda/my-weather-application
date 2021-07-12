f03ea53972d45a96ae428be669f4d459e097634f
fareesasyeda <82273133+fareesasyeda@users.noreply.github.com>
2021-07-12 11:53:02 -0400

    Displayed weather forecast

--- src/script.js
+++ src/script.js
@@ -41,40 +41,63 @@ function formatDate(placeHolderDate) {
 let date = new Date();
 formatDate(date);
 
-function getForecast(coordinates) {
-  console.log(coordinates.lat);
-  console.log(coordinates.lon);
-  let apiKey = "6cb3c244f40c2fba37f9f592c3aba492";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
+// Global variable to change between metric & imperial
+var globalUnit = "imperial";
+
+function formatDay(time) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let date = new Date(time * 1000);
+  let day = date.getDay();
+  return days[day];
 }
-function displayForecast() {
+
+function showForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-        <div class="col-2 border rounded shadow">
-          <div class="weather-forecast-date">
-          ${day} </div>
-         <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="36" /> <br />
-          <div class ="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max">24°</span>
-            <span class="weather-forecast-temperature-min"> 18°</span>
-            </div>
-        </div></div>`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">${Math.round(
+            forecastDay.temp.min
+          )}</span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temp.max
+          )}</span>
+        </div>
+      </div>
+  `;
+    }
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
 
-//challenge 2 change name of city based on user input value
+function forecastAPI(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6cb3c244f40c2fba37f9f592c3aba492";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${globalUnit}`;
+  axios.get(apiUrl).then(showForecast);
+}
+
 function showTemperature(response) {
-  //console.log(response);
+  console.log(response);
   console.log(response.data);
   //console.log(response.data.name);
   //console.log(response.data.main.temp);
@@ -87,6 +110,7 @@ function showTemperature(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
   let iconElement = document.querySelector("#weather-icon");
@@ -97,11 +121,11 @@ function showTemperature(response) {
 
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  getForecast(response.data.coord);
+  forecastAPI(response.data.coord);
 }
 
-function changeCity(event) {
-  event.preventDefault();
+function changeCity(city) {
+  //event.preventDefault();
   //?let insideSubmitFormButton = document.querySelector("#city-input-value");
   //?let insideUserCity = document.querySelector("#city-name");
   //?insideUserCity.innerHTML = insideSubmitFormButton.value;
@@ -115,48 +139,50 @@ function changeCity(event) {
   //let cityInput = document.querySelector("#city-input-value");
   //let city = cityInput.value;
   //combining 2 steps in 1
-  let city = document.querySelector("#city-input-value").value;
+  //let city = document.querySelector("#city-input-value").value;
   //can change this to metric
-  let units = "imperial";
-  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${globalUnit}`;
   console.log(apiUrl);
   console.log(axios);
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+  //axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+  axios.get(apiUrl).then(showTemperature);
   //}
+}
+
+function submitClicked(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input-value").value;
+  changeCity(city);
 }
 
 let submitFormButton = document.querySelector("#submit-button");
 //submit type or click type???
-submitFormButton.addEventListener("click", changeCity);
+submitFormButton.addEventListener("click", submitClicked);
 
-displayForecast();
-//week 5
-//3
-//function showTemperature(response) {
-//console.log(response);
-//}
-//1
-
-//bonus change from celcius to fahrenheit and viceversa
-//?
-//function celciusTemp(event) {
-//event.preventDefault();
-//let currentTemp = document.querySelector("#current-temp");
-//let conv = (currentTemp.value - 32) * 0.5555;
-//currentTemp.innerHTML = conv;
-//}
 function convertToCelciusTemp(event) {
   event.preventDefault();
-  let currentTemp = document.querySelector("#current-temp");
-  let celciusTemp = (fahrenheitTemperature - 32) * (5 / 9);
-  currentTemp.innerHTML = Math.round(celciusTemp);
+  globalUnit = "metric";
+  document.getElementById("celcius-conversion").style.color = "black";
+  document.getElementById("fahrenheit-conversion").style.color = "#0d6efd";
+  //let currentTemp = document.querySelector("#current-temp");
+  //let celciusTemp = (fahrenheitTemperature - 32) * (5 / 9);
+  //currentTemp.innerHTML = Math.round(celciusTemp);
+  let city = document.querySelector("#city-input-value").value;
+  changeCity(city);
+  document.querySelector("#windunit").innerHTML = "m/s";
 }
 
 function convertToFahrenheitTemp(event) {
   event.preventDefault();
-  let currentTemp = document.querySelector("#current-temp");
-  let fahrenheitTemp = fahrenheitTemperature;
-  currentTemp.innerHTML = Math.round(fahrenheitTemp);
+  globalUnit = "imperial";
+  document.getElementById("celcius-conversion").style.color = "#0d6efd";
+  document.getElementById("fahrenheit-conversion").style.color = "black";
+  //let currentTemp = document.querySelector("#current-temp");
+  //let fahrenheitTemp = fahrenheitTemperature;
+  //currentTemp.innerHTML = Math.round(fahrenheitTemp);
+  let city = document.querySelector("#city-input-value").value;
+  changeCity(city);
+  document.querySelector("#windunit").innerHTML = "mph";
 }
 
 let fahrenheitClick = document.querySelector("#fahrenheit-conversion");
@@ -166,3 +192,5 @@ let celciusClick = document.querySelector("#celcius-conversion");
 celciusClick.addEventListener("click", convertToCelciusTemp);
 
 let fahrenheitTemperature = null;
+
+changeCity("Arlington");
